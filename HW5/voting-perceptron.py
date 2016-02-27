76aecf4a82f91826c4569798cd20f53c372a4e95
Yuanchao Zhu <zyc262626@gmail.com>
2016-02-26 22:23:49 -0800

    HW5 modify

--- HW5/voting-perceptron.py
+++ HW5/voting-perceptron.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import heapq
 
 def sign(x):
     if x >= 0:
@@ -8,21 +9,22 @@ def sign(x):
     else:
         return -1
 
-def weighted_majority(x, w, c):
+def weighted_majority(x, h):
     sum_c = 0
-    l = len(w) - 1
-    for j in range(1, l+1):
-        sum_c += c[j] * sign(np.dot(np.array(w[j]), x))
+    l = len(h)
+    for j in range(l):
+        sum_c += h[j][0] * sign(np.dot(np.array(h[j][1]), x))
     return sign(sum_c)
 
-def averaged_perceptron(w, c):
-    l = len(w) - 1
-    sum_c = np.zeros((3, ))
-    for j in range(1, l+1):
-        sum_c += c[j] * np.array(w[j])
-    return sum_c
+def averaged_perceptron(h):
+    l = len(h)
+    return 0
+#    sum_c = np.zeros((3, ))
+#    for j in range(1, l+1):
+#        sum_c += c[j] * np.array(w[j])
+#    return sum_c
 
-def predict(x, w, c):
+def predict(x, h):
     t = x.shape[0]
     z = np.zeros((t,))
 
@@ -30,14 +32,13 @@ def predict(x, w, c):
 
     for i in range(t):
         if mode == "a":
-            z[i] = weighted_majority(x[i], w, c)
+            z[i] = weighted_majority(x[i], h)
 
         elif mode == "c":
-            z[i] = weighted_majority(x[i], w, c)
-            w_ = averaged_perceptron(w, c)
+            w_ = averaged_perceptron(h)
             z[i] = sign(np.dot(w_, x[i]))
-        if i % 100 == 0:
-            print i, z[i]
+        #if i % 100 == 0:
+        #    print i, z[i]
     return z
 
 x_l = []
@@ -56,14 +57,19 @@ x_arr = np.array(x_l)
 x_arr = np.insert(x_arr, 2, 1, axis=1)
 y_arr = np.array(y_l)
 
-l = 1
-c_l = [-1,0]
-w_l = [[-1,-1,-1],[0,0,0]]
-
 data_n = x_arr.shape[0]
 
 x_arr_rand = np.zeros((data_n, 3))
 y_arr_rand = np.zeros((data_n, ))
+
+# Keep L different w
+L = 1000000
+
+#h = [(-1, [-1,-1,-1])]
+h = []
+
+w = [0,0,0]
+c = 0
 
 while T > 0:
     T = T - 1
@@ -75,32 +81,34 @@ while T > 0:
         y_arr_rand[i] = y_arr[t]
     
     for i in range(data_n):
-        if sign(np.dot(np.array(w_l[l]), x_arr_rand[i])) !=  y_arr_rand[i]:
-            w_l.append((np.array(w_l[l]) + y_arr_rand[i] * x_arr_rand[i]).tolist())
-            c_l.append(1)
-            l += 1
+        if sign(np.dot(np.array(w), x_arr_rand[i])) !=  y_arr_rand[i]:
+            if len(h) < L:
+                heapq.heappush(h, (c, w))
+            else:
+                heapq.heappush(h, (c, w))
+                heapq.heappop(h)
+            w = (np.array(w) + y_arr_rand[i] * x_arr_rand[i]).tolist()
+            c = 1
         else:
-            c_l[l] += 1
+            c += 1
 
-l_final = len(w_l) - 1
-
-print "c, w calculation finished"
+print "h calculation finished"
+print h
 
 X = x_arr[:, :2]
 Y = y_arr
-h = .1  # step size in the mesh
+hs = .03  # step size in the mesh
 
 # create a mesh to plot in
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                                     np.arange(y_min, y_max, h))
+xx, yy = np.meshgrid(np.arange(x_min, x_max, hs),
+                                     np.arange(y_min, y_max, hs))
 
 # Plot the decision boundary. For that, we will assign a color to each
 # point in the mesh [x_min, m_max]x[y_min, y_max].
 fig, ax = plt.subplots()
-#Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = predict(np.c_[xx.ravel(), yy.ravel(), np.ones(xx.ravel().shape[0])], w_l, c_l)
+Z = predict(np.c_[xx.ravel(), yy.ravel(), np.ones(xx.ravel().shape[0])], h)
 
 # Put the result into a color plot
 Z = Z.reshape(xx.shape)
